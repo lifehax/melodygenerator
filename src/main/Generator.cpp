99e71ab9c8fcc6e56cae87f5c5231cf9a899980e
Kevin Irving <kvnirving@gmail.com>
2016-06-14 23:31:46 -0600

    Updates

--- src/main/Generator.cpp
+++ src/main/Generator.cpp
@@ -2,12 +2,15 @@
 #include "Generator.h"
 #include "Song.h"
 #include "Type.h"
+#include "MIDITrack.h"
 
 void Generator::generate(int type) {
 	Type * songType = Type::Create(type);
 	songType->setOptionValues(this->optionValues);
 	Song * song = new Song(songType);
-	song->makeTrack();
+	MIDITrack * madeTrack = song->makeTrack();
+	madeTrack->saveMIDI(songType->getName());
+	
 }
 
 void Generator::setOptionValues(std::map<std::string, std::string> * optionValues) {
